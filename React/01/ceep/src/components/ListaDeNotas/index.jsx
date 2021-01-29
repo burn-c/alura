166446aa515e7b44354fbe3657538b8f138d34be
Carlos Oliveira <chopcsom@gmail.com>
2021-01-28 22:23:17 -0300

    Initial: CEEP

---
+++ React/01/ceep/src/components/ListaDeNotas/index.jsx
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+
+import CardNota from '../CardNota';
+
+import './style.css';
+
+class ListaDeNotas extends Component { 
+
+  render(){
+    return (
+      <ul className="lista-notas">
+        {this.props.notas.map((nota, index) => {
+            return (
+              <li className="lista-notas_item" key={index} >
+                <CardNota titulo={nota.titulo} text={nota.texto} />  
+              </li>
+            );
+          })
+        }
+      </ul>
+    );
+
+  }
+}
+
+export default ListaDeNotas;
