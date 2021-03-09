56c0b38a63f4a2c97aa288fb2bb7ab5514396fb1
Carlos Oliveira <carlos.burn@gmail.com>
2021-03-08 22:16:05 -0300

    feat: 9.3 - Interfaces

--- JS/bytebank/SistemaAutenticacao.js
+++ JS/bytebank/SistemaAutenticacao.js
@@ -1,5 +1,15 @@
+/**
+Ser autenticavel significa ter um método autenticar
+*/
+
 export class SistemaAutenticacao {
-  static login(funcionario, senha) {
-    return funcionario.senha == senha;
+  static login(autenticavel, senha) {
+    const estaLogado = autenticavel.autenticar(senha);
+
+    estaLogado
+      ? console.log(autenticavel._nome + " está logado! ✔️")
+      : console.log(autenticavel._nome + " não está logado! ✖️");
+
+    return estaLogado;
   }
 }
